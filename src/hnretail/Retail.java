cf43b76c88888eb575e4fd30b8afb2653a7f161e
abendezur <abendezur@UTIC_D1.enee.hn>
2016-08-22 15:56:27 -0600

    Initial Setup

---
+++ src/hnretail/Retail.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hnretail;
+
+/**
+ *
+ * @author abendezur
+ */
+public class Retail {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new hnretail.view.menu_frame().setVisible(true);
+    }
+    
+}
